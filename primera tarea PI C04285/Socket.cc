b85826bfbff7ae0776e2ed900f52649b8b84b8e8
DanielLM2002 <dlizano@outlook.com>
2022-08-23 10:37:44 -0600

    <se añade codigo incial>

---
+++ primera tarea PI C04285/Socket.cc
@@ -0,0 +1,91 @@
+#include "Socket.h"
+
+/* 
+   char tipo: el tipo de socket que quiere definir
+      's' para "stream
+      'd' para "datagram"
+   bool ipv6: si queremos un socket para IPv6
+ */
+Socket::Socket( char type, bool ipv6 ){
+
+}
+
+
+Socket::~Socket(){
+    Close();
+}
+
+
+void Socket::Close(){
+
+}
+
+/*
+   char * hostip: direccion del servidor, por ejemplo "www.ecci.ucr.ac.cr"
+   int port: ubicacion del proceso, por ejemplo 80
+ */
+int Socket::Connect( const char * hostip, int port ) {
+
+   return -1;
+
+}
+
+
+/*
+   char * hostip: direccion del servidor, por ejemplo "www.ecci.ucr.ac.cr"
+   char * service: nombre del servicio que queremos acceder, por ejemplo "http"
+ */
+int Socket::Connect( const char *host, const char *service ) {
+
+   return -1;
+
+}
+
+
+int Socket::Read( char *text, int len ) {
+
+   return -1;
+
+}
+
+
+int Socket::Write( const char *text ) {
+
+   return -1;
+
+}
+
+
+int Socket::Listen( int queue ) {
+
+    return -1;
+
+}
+
+
+int Socket::Bind( int port ) {
+
+    return -1;
+
+}
+
+
+Socket * Socket::Accept(){
+
+    return (Socket *) 0;
+
+}
+
+
+int Socket::Shutdown( int mode ) {
+
+    return -1;
+
+}
+
+
+void Socket::SetIDSocket(int id){
+
+    idSocket = id;
+
+}
---
+++ primera tarea PI C04285/Socket.h
@@ -0,0 +1,37 @@
+
+/*
+ *  Establece la definicion de la clase Socket para efectuar la comunicacion
+ *  de procesos que no comparten memoria, utilizando un esquema de memoria
+ *  distribuida.  El desarrollo de la clase se hara en dos etapas, primero
+ *  los metodos necesarios para los clientes, en la otra etapa los otros
+ *  metodos para los servidores
+ */
+
+#ifndef Socket_h
+#define Socket_h
+
+class Socket{
+
+    public:
+        Socket( char, bool = false );
+        Socket( int );
+        ~Socket();
+        int Connect( const char *, int );
+        int Connect( const char *, const char * );
+        void Close();
+        int Read( char *, int );
+        int Write( const char *, int );
+        int Write( const char * );
+        int Listen( int );
+        int Bind( int );
+        Socket * Accept();
+        int Shutdown( int );
+        void SetIDSocket( int );
+        
+    private:
+        int idSocket;
+        int port;
+        bool ipv6;
+};
+
+#endif
