91caa1720301644471d3f793d7d9bb2b65c33475
Daniel Lizano <dlizano@outlook.com>
2022-08-26 11:41:36 -0600

    se agrega while para leer todo el txt

--- primera tarea PI C04285/ipv4-test.cc
+++ primera tarea PI C04285/ipv4-test.cc
@@ -14,14 +14,13 @@
 int main( int argc, char * argv[] ) {
    const char * os = "http://os.ecci.ucr.ac.cr/";
    const char * osn = "163.178.104.187";
-   const char * request = "GET /futbol/2018/ HTTP/1.1\r\nhost: redes.ecci\r\n\r\n";
+   const char * request = "GET /futbol/2018/clasificacion.txt HTTP/1.1\r\nhost: redes.ecci\r\n\r\n";
 
    Socket s( 's' );	// Create a new stream socket for IPv4
    char a[512];
-
    s.Connect( osn, 80 );
    s.Write(  request );
-   s.Read( a, 512 );
-   printf( "%s\n", a);
-
+   while(s.Read(a,512)!= 0){
+      printf( "%s\n", a);
+   }
 }
